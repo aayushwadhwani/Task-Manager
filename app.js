6edc19bc6752ea886ebbea899cb0f19eba6bc28e
Aayush Wadhwani <aayushwadhwani99@gmail.com>
2021-11-08 00:29:27 +0530

    :construction: routes for better structure

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const tasks = require('./routes/tasks');
 const port = 5000;
 
 const app = express();
 
+//middlewares
+app.use(express.json());//we will send json as the body...
+
 app.get('/',(req,res)=>{
     res.status(200).send('Task Manager App');
 });
+app.use('/api/v1/tasks',tasks);
 
 app.listen(port,()=>{
     console.log(`Listening on port ${port}...`);
