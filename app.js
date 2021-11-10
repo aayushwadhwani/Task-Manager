c26159b89e748c98a199dba063dce4ef71d61617
Aayush Wadhwani <aayushwadhwani99@gmail.com>
2021-11-10 21:25:53 +0530

    Better approach to connect DB

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const connectDB = require('./db/connect');
 const express = require('express');
+require('dotenv').config();
 const tasks = require('./routes/tasks');
 const port = 5000;
 
@@ -12,6 +14,15 @@ app.get('/',(req,res)=>{
 });
 app.use('/api/v1/tasks',tasks);
 
-app.listen(port,()=>{
-    console.log(`Listening on port ${port}...`);
-});
+const runApp = async() => {
+    try{
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port,()=>{
+            console.log(`Listening on port ${port}...`);
+        });
+    }catch(err){
+        console.log(err);
+    }
+}
+runApp();
+
